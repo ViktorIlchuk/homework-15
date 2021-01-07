d2d5f7ba758f5f41548dde120742016757200bab
Admin <admin@MacBook-Pro-Admin.local>
2021-01-07 19:39:15 +0200

    fixed bug with approval icon

--- src/Post/Post.js
+++ src/Post/Post.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import approvalIcon from '../img/approvalIcon.png'
 import './Post.css';
 
 function Post(props) {
-    console.log(props)
     return (
         <div className='Post'>            
             <img src={props.author.photo} className='avatar' alt='avatar' />
             <div className='postInfo'>
                 <div className='authorInfo'>
                     <p className='authorName'>{props.author.name}</p>
-                    <img className='active' src='/active.png' alt='public' />
+                    <img className='active' src={approvalIcon}  alt='approval' />
                     <p className='nickName'>{props.author.nickname}</p>                   
                     <p className='postDate'>{props.date}</p>
                 </div>
